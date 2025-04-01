f76e28d294640b7063bf0a1be033774b62b51cbc
nic00la1 <nic00la.kaleta@gmail.com>
2025-04-01 12:56:56 +0200

    Dodaj pliki projektów.

---
+++ ToolsManagmentMAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ToolsManagmentMAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
